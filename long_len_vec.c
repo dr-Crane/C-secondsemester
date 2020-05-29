49ab3c94ecb23d85b63330ed6bbf165ab0891b90
Bekzat <61666791+dr-Crane@users.noreply.github.com>
2020-05-30 02:05:22 +0700

    Update long_len_vec.c

--- long_len_vec.c
+++ long_len_vec.c
@@ -5,18 +5,17 @@
 /*
  * Выделяю память и зануляю все разряды.
  * К примеру:
- * Необходимо 100 разрядов. (100/8)+1 = 13, поэтому будет выделено 13 ячеек (элементов массива где каждая имеет 8 бит),
- * которые в свою очередь сбрасываются до нуля.
+ * Необходимо 100 разрядов. (99/8)+1 = 13 (берём целочисленно),
+ * поэтому будет выделено 13 ячеек (элементов массива где каждая имеет 8 бит).
  */
 unsigned char *give_memory(int num)
 {
-    unsigned char *arr = (unsigned char*) malloc(sizeof(unsigned char)*((num/8)+1));
-    for(int i=0; i<((num/8)+1); i++)    // Идём по массиву
+    unsigned char *arr = (unsigned char*) malloc(sizeof(unsigned char)*(((num-1)/8)+1));
+    if(!arr) return NULL;
+    arr[(((num-1)/8)+1)] = '\0';
+    for(int i=0; i<(((num-1)/8)+1); i++)
     {
-        for(int j=0; j<8; j++)          // Выполняем действия над ячейкой
-        {
-            arr[j] = arr[j]^arr[j]; // May be some errors. Try to check it by debugger. Clang says, that it will equall to 0.
-        }
+        arr[i] = 0;
     }
     return arr;
 }
@@ -29,14 +28,14 @@ unsigned char *fill_vec_by_arr(char *str)
     unsigned char *arr = give_memory(string_len);
     if(!arr) return NULL;
     int current = 0;
-    for(int i=0; i<string_len; i++)
+    for(int i=0;(string_len-i-1)>=0; i++)
     {
         unsigned char mask = 1;
-        for (int j=0; j<8; j++)
+        for (int j=0; (j<8)&&(current<string_len); j++)
         {
-            if(str[current]!='0')
+            if(str[string_len-i-1]!='0')
             {
-                arr[i] = arr[i]|mask;
+                arr[(((string_len-1)/8)+1)-1-i] = arr[(((string_len-1)/8)+1)-i-1]|mask;
             }
             mask = mask<<1;
             current++;
@@ -45,25 +44,18 @@ unsigned char *fill_vec_by_arr(char *str)
     return arr;
 }
 
-void showvec(unsigned char *arr)
-{
-    for(int i=0; i<strlen(arr); i++)
-    {
-        unsigned char mask = 1;
-        for (int j = 0; j < 8; j++)
-        {
-            if(arr[i] == (arr[i]|mask)) printf("1");
-            else printf("0");
-            mask = mask<<1;
-        }
-    }
-}
+//void showvec(unsigned char *arr)
+//{
+//    int arr_len = strlen(arr);
+//    for(int i=0; i<arr_len; i++)
+//    {
+//        for(int j=0; j)
+//    }
+//}
 
 int main()
 {
-    char arr[] = "1001001010";
-    unsigned char *filled_vec = fill_vec_by_arr(arr);
-    showvec(filled_vec);
+    char arr[] = "111111111111111111111111111111111111";
 
     return 0;
 }
