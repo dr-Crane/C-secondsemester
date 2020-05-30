fbf9ec3aeba4354858a8924a804d1f8f1b0916e8
Bekzat <61666791+dr-Crane@users.noreply.github.com>
2020-05-30 17:45:54 +0700

    Update long_len_vec.c

--- long_len_vec.c
+++ long_len_vec.c
@@ -20,7 +20,8 @@ unsigned char *give_memory(int num)
     return arr;
 }
 /*
- * Заполняем массив нужным нам образом. В каждой ячейки отсчёт ведём справа налево.
+ * Заполняем массив (вектор) нужным нам образом. В каждой ячейки отсчёт ведём справа налево. Во втором цикле составное условие,
+ * поэтому цикл выполняется либо до тех пор пока j<8 или пока не закончится строка.
  */
 unsigned char *fill_vec_by_arr(char *str)
 {
@@ -43,30 +44,43 @@ unsigned char *fill_vec_by_arr(char *str)
     return arr;
 }
 
-void showvec(unsigned char *arr, int len_of_string)
+
+/*
+ * Конвертируем вектор (массив) в строку . Алгоритм обработки тот же самый, что был показан выше. Рассматриваем
+ * бинарную составляющую каждой ячейки и в заисимости от состояния бита записываем всё в массив.
+ */
+char *vec_to_arr(unsigned char *arr, int len_of_string)
 {
-    int arr_len = strlen(arr);
-    int current =0;
-    for(int i=0; i<arr_len; i++)
+    char *string = (char*) malloc(len_of_string+1);
+    if(!string) return NULL;
+    int current=0;
+    string[len_of_string] = '\0';
+    for(int i=0; i<strlen(arr); i++)
     {
         unsigned char mask = 1;
         for(int j=0; j<8&&(current<len_of_string); j++)
         {
-            if(arr[i] == (arr[i]|mask)) printf("1");
-            else printf("0");
+            if(arr[i]==(arr[i]|mask)) string[current]='1';
+            else string[current]='0';
             current++;
             mask = mask<<1;
         }
-
     }
+    return string;
 }
+
+
 
 int main()
 {
     char arr[] = "1010010100010100101010100101011010010100101";
 
     unsigned char *vec = fill_vec_by_arr(arr);
-    showvec(vec, strlen(arr));
+
+    char *string = vec_to_arr(vec, strlen(arr));
+
+    for(int i=0; i<strlen(arr); i++)
+        printf("%c", string[i]);
 
     return 0;
 }
