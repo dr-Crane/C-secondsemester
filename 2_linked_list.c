26e4bd1d581edd9258326172dd82c8641b95e502
Bekzat <61666791+dr-Crane@users.noreply.github.com>
2020-06-02 12:29:24 +0700

    Update 2_linked_list.c

--- 2_linked_list.c
+++ 2_linked_list.c
@@ -8,6 +8,17 @@ struct node
     struct node *prev;
 };
 
+struct table
+{
+    struct node *reference;
+    struct node *other;
+};
+
+/*
+ * Функция создаёт новый узел списка и ставит его в начало, причём если список пуст, то тот элемент и будет началом списка.
+ * Параметрами являются указатель на список, "головой" которого должен стать новый элемент и информация для заполнения поля,
+ * функция возвращает указатель на новый список.
+*/
 struct node *addtoHead(struct node *list, int x)
 {
     struct node *ptr = (struct node*)malloc(sizeof(struct node));
@@ -25,7 +36,10 @@ struct node *addtoHead(struct node *list, int x)
     return list;
 
 }
-
+/*
+ * Функция создаёт и добавляет новый элемент в конец списка. Для этого в чикле ищем элемент указатель после которого равен NULL.
+ * После добавляем этот самый элемент. Возвращает  указатель на список.
+ */
 struct node *addtoTail(struct node *list, int x)
 {
     struct node *ptr = (struct node*)malloc(sizeof(struct node));
@@ -55,6 +69,11 @@ struct node *addtoTail(struct node *list, int x)
     }
 }
 
+/*
+ *  Функция добавляет элемнт перед заданным. srch- элемент списка, после которого нужно добавить новый элемент,
+ *  х- значение нового узла. Создаём новый узел и заполняем поля, после выполняем поиск в цикле пока не находим
+ *  нужный нам элемент и добавляем его перед ним. Функция возвращает указатель на начало нового списка.
+ */
 struct node *addBefore(struct node *list, int x, int srch)
 {
 
@@ -95,6 +114,9 @@ struct node *addBefore(struct node *list, int x, int srch)
     }
 }
 
+/*
+ * Функция считает количество узлов списка.
+ */
 int listlen(struct node *list)
 {
     int count = 0;
@@ -106,6 +128,10 @@ int listlen(struct node *list)
     return count;
 }
 
+/*
+ *  Функция поиска узла в списке. В парамметрах передаём адрес начала списка, и значение которое необходимо найти.
+ *  В случае если значение не было найдено возвращает NULL, иначе возвращает указатель на найденный узел.
+ */
 struct node *findNode(struct node *list, int x)
 {
     while (list)
@@ -116,6 +142,9 @@ struct node *findNode(struct node *list, int x)
     return NULL;
 }
 
+/*
+ * Функция печати списка.
+ */
 void printList(struct node *list)
 {
     while (list)
@@ -123,8 +152,15 @@ void printList(struct node *list)
         printf("%d ", list->key);
         list = list->next;
     }
+    printf("\n");
 }
 
+/*
+ * Функция создаёт и устанавливает новый узел в заданную позицию, причём  позиция должна быть числом натуральным.
+ * В случае какой либо ошибки, а именно установки в не существующую позицию (если количество элементов меньше
+ * запрашиваемой позиции) или любого другого несоответсвтия (к примеру не выделилась память) возвращает NULL, иначе
+ * указатель на начало нового списка.
+ */
 struct node *addPos(struct node *list, int x, int pos)
 {
     if(pos<=0) return NULL;
@@ -170,6 +206,9 @@ struct node *addPos(struct node *list, int x, int pos)
     return NULL;
 }
 
+/*
+ * Удаляет все элементы списка и возвращает NUll.
+ */
 struct node *deleteList(struct node *list)
 {
     if(list == NULL) return NULL;
@@ -186,6 +225,10 @@ struct node *deleteList(struct node *list)
     return NULL;
 }
 
+/*
+ * Добавляет новый элемент после искомого параметра. В случае если такого элемегта нет, возвращает NULL.
+ * Поиск ведём в цикле. Возвращает указатель на начало нового списка.
+ */
 struct node *addAfter(struct node *list, int x, int srch)
 {
     struct node *ptr = (struct node*)malloc(sizeof(struct node));
@@ -224,6 +267,9 @@ struct node *addAfter(struct node *list, int x, int srch)
     }
 }
 
+/*
+ * Удаляет "голову" списка, и возвращает указатель на начало нового.
+ */
 struct node *deleteHead(struct node *list)
 {
     struct node *head = list;
@@ -233,6 +279,9 @@ struct node *deleteHead(struct node *list)
     return list;
 }
 
+/*
+ * Удаляет "хвост" и возвращает указатель на начало нового списка.
+ */
 struct node *deleteTail(struct node *list)
 {
     if(list == NULL) return NULL;
@@ -256,6 +305,10 @@ struct node *deleteTail(struct node *list)
     return NULL;
 }
 
+/*
+ * Удаляет узел после элемента заданного в параметре, в случае неудачи (элемент не был найден) возвращает NULL. Иначе
+ * возвращает указатель на новый список.
+ */
 struct node *deleteAfter(struct  node *list, int x)
 {
     if(list==NULL) return NULL;
@@ -290,6 +343,9 @@ struct node *deleteAfter(struct  node *list, int x)
     return NULL;
 }
 
+/*
+ * Удаляет узел перед заданным параметром.
+ */
 struct node *deleteBefore(struct  node *list, int x)
 {
     if(list==NULL) return NULL;
@@ -320,6 +376,9 @@ struct node *deleteBefore(struct  node *list, int x)
     return NULL;
 }
 
+/*
+ * Удаляет элемент из заданной позиции. В случае отсутсвия позиции возвращает NULL, иначе указатель на новый список.
+ */
 struct node *deletePos(struct node *list, int pos)
 {
     if(list==NULL) return NULL;
@@ -353,16 +412,66 @@ struct node *deletePos(struct node *list, int pos)
     return NULL;
 }
 
+/*
+ * Функция делит список на два. В одном уникальные, а вдругом прочие элементы с сохранением порядка. Для неё я создал
+ * новую структуру table с полями: reference и other. После свёл задачу к решённой использую addtoTail и deleteHead.
+ */
+struct table separate(struct node *head)
+{
+    struct table separated;
+    separated.reference=NULL;
+    separated.other = NULL;
+    int space = 0 ;
+    struct node *ptr = head;
+    while (ptr)
+    {
+        int condition = 0;
+        space = ptr->key;
+        for(struct node *copy = separated.reference; copy!=NULL; copy = copy->next) // Поиск элемента среди "особенных"
+        {
+            if(space == copy->key)
+            {
+                condition = 1;                                                      // Если такой элемент уже был
+            }                                                                       // То изменяем значение condition
+        }
+        if(condition)
+        {
+            separated.other = addtoTail(separated.other, space);
+        } else
+        {
+            separated.reference = addtoTail(separated.reference, space);
+        }
+        ptr = deleteHead(ptr); 
+    }
+    return separated;
+}
+
+
 int main()
 {
     struct node *head = NULL;
-    for(int i=0; i<10; i++)
-    {
-        head = addtoHead(head, i);
-    }
+    head = addtoTail(head, 2);
+    head = addtoTail(head, 0);
+    head = addtoTail(head, 5);
+    head = addtoTail(head, 6);
+    head = addtoTail(head, 3);
+    head = addtoTail(head, 2);
+    head = addtoTail(head, 2);
+    head = addtoTail(head, 4);
+    head = addtoTail(head, 5);
+    head = addtoTail(head, 6);
+    head = addtoTail(head, 0);
+    head = addtoTail(head, 0);
+    printf("List: ");
     struct node *ptr = head;
-    ptr = deleteBefore(ptr, 12);
-    if(ptr==NULL) printf("Null\n");
     printList(ptr);
+
+    struct table separated = separate(ptr);
+    printf("Reference:");
+    printList(separated.reference);
+    printf("Other:");
+    printList(separated.other);
+
+
 
 }
