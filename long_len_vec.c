d39843e868bd22c8c510283acab65b1ecf4d5b29
Bekzat <61666791+dr-Crane@users.noreply.github.com>
2020-06-04 16:40:56 +0700

    Update long_len_vec.c

--- long_len_vec.c
+++ long_len_vec.c
@@ -84,7 +84,9 @@ unsigned char *invert_vec(unsigned char *vec, int len_vec)
 }
 
 /*
- * Функция выполняет логическое сложение.
+ * Функция выполняет логическое сложение. Складываю до тех пор пока не закончатся разряды у меньшего вектора.
+ * В случае если они равны просто верну получившийся вектор иначе перепишу значения из большего вектора в
+ * новый вектор.
  */
 unsigned char *logic_summ(unsigned char *vec_1, unsigned char *vec_2, int len_1, int len_2)
 {
@@ -102,14 +104,94 @@ unsigned char *logic_summ(unsigned char *vec_1, unsigned char *vec_2, int len_1,
          min = cells_1;
     }
     unsigned char *arr = (unsigned char *) malloc(sizeof(unsigned char)*max);
+    if(!arr) return NULL;
     for(int i=0; i<max; i++)
     {
         arr[i] = 0;
     }
-    if(!arr) return NULL;
     for(int i=0; i<min; i++) // Складываю до тех пор пока не закончатся ячейки у меньшего вектора
     {
         arr[i] = vec_1[i]|vec_2[i];
+        current++;
+    }
+    if(min == max) return arr;
+    if(cells_1>cells_2)
+    {
+        for(;current<max; current++)
+        {
+            arr[current] = vec_1[current];
+        }
+    } else
+    {
+        for(;current<max; current++)
+        {
+            arr[current] = vec_2[current];
+        }
+    }
+    return arr;
+}
+
+/*
+ * Логическое умножение векторов. В случае если длины векторов не равны, то разряды , которых нет у меньшего вектора,
+ * но есть у возвращаемого вектора (т.к. масси из кучи мы выделяем размерности большего вектора) просто обнуляю.
+ */
+unsigned char *logic_mult(unsigned char *vec_1, unsigned char *vec_2, int len_1, int len_2)
+{
+    if(!vec_2||!vec_1) return NULL;
+    int cells_1 = ((len_1-1)/8)+1;
+    int cells_2 = ((len_2-1)/8)+1;
+    int max=0, min=0;
+    if(cells_1>cells_2)
+    {
+        max = cells_1;
+        min = cells_2;
+    } else
+    {
+        max = cells_2;
+        min = cells_1;
+    }
+    unsigned char *arr = (unsigned char *) malloc(sizeof(unsigned char)*max);
+    if(!arr) return NULL;
+    for(int i=0; i<max; i++)
+    {
+        arr[i] = 0;
+    }
+    for(int i=0; i<min; i++)
+    {
+        arr[i] = vec_1[i]&vec_2[i];
+    }
+    return arr;
+}
+
+/*
+ * Функция выполняет сложение по модулю два. Алгоритм абослютно тот же самый, что и в logic_summ() за исключением
+ * arr[i] = vec_1[i]^vec_2[i]; на 194 строчке. Так же как в logic_summ() в случае если длины векторов не равны, то
+ * значения из большего вектора перепишу в новый вектор.
+ */
+unsigned char *logic_xor(unsigned char *vec_1, unsigned char *vec_2, int len_1, int len_2)
+{
+    if(!vec_2||!vec_1) return NULL;
+    int cells_1 = ((len_1-1)/8)+1;
+    int cells_2 = ((len_2-1)/8)+1;
+    int max=0, min=0, current=0;
+    if(cells_1>cells_2)
+    {
+        max = cells_1;
+        min = cells_2;
+    } else
+    {
+        max = cells_2;
+        min = cells_1;
+    }
+    unsigned char *arr = (unsigned char *) malloc(sizeof(unsigned char)*max);
+    if(!arr) return NULL;
+    for(int i=0; i<max; i++)
+    {
+        arr[i] = 0;
+    }
+    for(int i=0; i<min; i++)
+    {
+        arr[i] = vec_1[i]^vec_2[i];
         current++;
     }
     if(min == max) return arr;
@@ -152,4 +234,15 @@ int main()
 //    unsigned char *l_summ = logic_summ(vec_1, vec_2, 45, 45);
 //    show_vec(l_summ, 45);
 
+    // Пример для logic_mult()
+//    printf("Log mult:");
+//    unsigned char *l_mult = logic_mult(vec_2, vec_1, 45, 45);
+//    show_vec(l_mult, 45);
+
+    // Пример для logic_xor()
+//    printf("Log xor :");
+//    unsigned char *l_xor = logic_xor(vec_1, vec_2, 45,45);
+//    show_vec(l_xor, 45);
+
+    
 }
